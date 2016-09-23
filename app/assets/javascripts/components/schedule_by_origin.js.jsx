0f0fdc4e37111e46c148b816265a168fd3475135
Mike Lumetta <mjlumetta@gmail.com>
2016-09-23 03:57:39 +0000

    Implement show/hide functionality for individual stations

--- app/assets/javascripts/components/schedule_by_origin.js.jsx
+++ app/assets/javascripts/components/schedule_by_origin.js.jsx
@@ -3,6 +3,10 @@ var ScheduleByOrigin = React.createClass({
     return { showTrains: true };
   },
   
+  showOrHideDepartures() {
+    this.setState({ showTrains: !this.state.showTrains });
+  },
+  
   render() {
     var departureLines = this.props.trains.map(
       function(train) {
@@ -11,12 +15,19 @@ var ScheduleByOrigin = React.createClass({
         );
       }, this
     );
+    var glyphiconArrow = this.state.showTrains ? "up" : "down";
+    var glyphiconClass = "glyphicon glyphicon-menu-" + glyphiconArrow;
     
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
           <h4>
-            Trains departing from <strong>{this.props.origin}</strong> <span className="badge">{this.props.trains.length}</span>
+            Trains departing from <strong>{this.props.origin}</strong>&nbsp;
+            <span className="badge">{this.props.trains.length}</span>
+            <span className="float-right" onClick={this.showOrHideDepartures}>
+              <span className={glyphiconClass} aria-hidden="true" />
+              &nbsp;{ this.state.showTrains ? "Hide" : "Show" }
+            </span>
           </h4>
         </div>
         
